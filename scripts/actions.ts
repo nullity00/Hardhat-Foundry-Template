dd282a2cad4277d54c707a4ccadc3570b6bc91e0
TheReshma <reshmashaik3011@gmail.com>
2023-01-03 01:28:44 +0530

    multi sig

--- scripts/actions.ts
+++ scripts/actions.ts
@@ -1,13 +1,16 @@
 const h = require("hardhat");
 
 async function main() {
-  const greeter = await h.ethers.getContractAt(
-    "Greeter",
-    "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  const multiSig = await h.ethers.getContractAt(
+    "MultiSig",
+    "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
   );
 
-  const res = await greeter.greet();
-  console.log(res);
+  const amount = h.ethers.utils.parseEther("0.01");
+
+  // const res = await multiSig.submitTransaction("0x70997970C51812dc3A010C7d01b50e0d17dc79C8", amount, "0x");
+  const res2 = await multiSig.getTransactions();
+  console.log(res2);
 }
 
 main().catch((error) => {
