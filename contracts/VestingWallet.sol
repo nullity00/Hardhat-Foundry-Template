c893ed139dbbc5b3eda3de890b77a6c04b2e169b
nullity00 <reshmashaik3011@gmail.com>
2023-04-27 04:35:43 +0530

    tracing txns using hardhat tracer

--- contracts/VestingWallet.sol
+++ contracts/VestingWallet.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/finance/VestingWallet.sol";
 
+import "hardhat/console.sol";
+
 contract Investing is VestingWallet {
 
   constructor(
     address beneficiaryAddress,
     uint64 startTimestamp,
     uint64 durationSeconds
-  ) VestingWallet(beneficiaryAddress, startTimestamp, durationSeconds) {}
+  ) payable VestingWallet(beneficiaryAddress, startTimestamp, durationSeconds) {}
 
 }
--- test/test-vesting-wallet.js
+++ test/test-vesting-wallet.js
@@ -1,29 +1,52 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Vesting Wallet", function () {
-  let deployer, user1, user2, user3;
+  let deployer, beneficiery;
+
+  const DURATION = 60 * 60 * 24 * 30; // 30 days
 
   before(async () => {
-    [deployer, user1, user2, user3] = await ethers.getSigners();
+    [deployer, beneficiery] = await ethers.getSigners();
 
     const vestingwallet = await ethers.getContractFactory(
       "contracts/VestingWallet.sol:Investing",
       deployer
     );
-    this.vestingWalletContract = await vestingwallet.deploy([
-      deployer.address,
-      user1.address,
-      user2.address,
-    ]);
+
+    const block = await ethers.provider.getBlock();
+    const duration = ethers.BigNumber.from(DURATION);
+    this.vestingWalletContract = await vestingwallet.deploy(
+      beneficiery.address,
+      block.timestamp,
+      duration,
+      {
+        value: ethers.utils.parseEther("1"),
+      }
+    );
     await this.vestingWalletContract.deployed();
+    this.initialBalanceBeneficiery = await ethers.provider.getBalance(
+      beneficiery.address
+    );
   });
 
   it("Actions", async () => {
-
+    await time.increase(DURATION);
+    const tx = await this.vestingWalletContract["release()"]();
   });
 
   after(async () => {
+    const balance = await ethers.provider.getBalance(
+      this.vestingWalletContract.address
+    );
+    const balanceBeneficiery = await ethers.provider.getBalance(
+      beneficiery.address
+    );
 
+    expect(balance).to.equal(ethers.utils.parseEther("0"));
+    expect(balanceBeneficiery).to.equal(
+      this.initialBalanceBeneficiery.add(ethers.utils.parseEther("1"))
+    );
   });
 });
